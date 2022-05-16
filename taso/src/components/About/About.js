112ccd0f782433c42207b874bafe72a8af1d9d50
Hoang Nhat Duy Nguyen <hoangnhatduy.nguyen@tuni.fi>
2022-05-16 14:56:25 +0300

    Add images, add modal layout

--- taso/src/components/About/About.js
+++ taso/src/components/About/About.js
@@ -1,37 +1,19 @@
 import React from 'react'
 import './About.css'
+import Image from '../../images/card-image-top.png'
 
 const About = () => {
     return (
         <div className="container">
             <div className="row">
-                <div className="col-sm-4 col-md-4 item">
-                    <div className="card">
-                        <div className="card-body">
-                            <p className="card-text">
-                                Tampereen sähköopiskelijat (TASO ry) on Tampereen ammattikorkeakoulun sähkö- ja automaatiotekniikan opiskelijoiden ainekerho.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-sm-4"></div>
-                <div className="col-sm-4 col-md-4 item">
-                    <div className="card">
-                        <div className="card-body">
-                            <p className="card-text">
-                                TASO ry on Tampereen ammattikorkeakoulun (TAMK) opiskelijakunnan (TAMKO) alayhdistys.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className='row'>
-                <div className="col-md-12">
-                    <div className="card2">
-                        <div className="card-body">
-                            <p className="card-text">
-                                Valtaosa kerhon jäsenistä takaisin sähkö- ja automaatiotekniikan opiskelijoista, mutta TASOn jäseneksi saa liittyä kuka vain TAMKin opiskelijoista, josta johtuen monet eri koulutusohjelmista ovat liittyneet TASOn vahvuuteen.                            </p>
-                        </div>
+                <div className="col-md-7">
+                    <div className="aboutContent">
+                        <p>Tampereen sähköopiskelijat (TASO ry) on Tampereen ammattikorkeakoulun sähkö- ja automaatiotekniikan opiskelijoiden ainekerho.
+                            <br />TASO ry on Tampereen ammattikorkeakoulun (TAMK) opiskelijakunnan (TAMKO) alayhdistys.
+                        </p>
+                        <p>
+                            Valtaosa kerhon jäsenistä takaisin sähkö- ja automaatiotekniikan opiskelijoista, mutta TASOn jäseneksi saa liittyä kuka vain TAMKin opiskelijoista, josta johtuen monet eri koulutusohjelmista ovat liittyneet TASOn vahvuuteen.
+                        </p>
                     </div>
                 </div>
             </div>
@@ -41,99 +23,135 @@ const About = () => {
                     <div className="title" style={{ textAlign: 'center' }}>
                         <h1>Jäsenet</h1>
                     </div>
-                    <div className="card-group" style={{ marginTop: '50px' }}>
-                        <div class="cardMem">
-                            <img src="..." class="card-img-top" alt="..." />
-                            <div class="card-body">
-                                <h5 class="card-title">Matti Hämelahti</h5>
-                                <p class="card-textMem">
-                                    Puheenjohtaja<br />
-                                    Email: xxxx@tuni.fi
-                                </p>
+                    <div className="card-group-members">
+                        <div className="cardMem">
+                            <div className='face face1'>
+                                <div className="memberContent">
+                                    <img src={Image} className="img-fluid rounded-start" alt="..." />
+                                    <h3>Matti Hämelahti</h3>
+                                </div>
+                            </div>
+                            <div className='face face2'>
+                                <div className="memberContent">
+                                    <p>Puheenjohtaja<br />
+                                        Email: xxxx@tuni.fi</p>
+                                </div>
                             </div>
                         </div>
-                        <div class="cardMem">
-                            <img src="..." class="card-img-top" alt="..." />
-                            <div class="card-body">
-                                <h5 class="card-title">Juuse Hannula</h5>
-                                <p class="card-textMem">
-                                    Varapuheenjohtaja<br />
-                                    Email: xxxx@tuni.fi
-                                </p>
+                        <div className="cardMem">
+                            <div className='face face1'>
+                                <div className="memberContent">
+                                    <img src={Image} className="img-fluid rounded-start" alt="..." />
+                                    <h3>Juuse Hannula</h3>
+                                </div>
+                            </div>
+                            <div className='face face2'>
+                                <div className="memberContent">
+                                    <p>Varapuheenjohtaja<br />
+                                        Email: xxxx@tuni.fi</p>
+                                </div>
                             </div>
                         </div>
-                        <div class="cardMem">
-                            <img src="..." class="card-img-top" alt="..." />
-                            <div class="card-body">
-                                <h5 class="card-title">Samuli Marttila</h5>
-                                <p class="card-textMem">
-                                    Rahastonhoitaja<br />
-                                    Email: xxxx@tuni.fi
-                                </p>
+                        <div className="cardMem">
+                            <div className='face face1'>
+                                <div className="memberContent">
+                                    <img src={Image} className="img-fluid rounded-start" alt="..." />
+                                    <h3>Samuli Marttila</h3>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                    <div className="card-group" style={{ marginTop: '50px' }}>
-                        <div class="cardMem">
-                            <img src="..." class="card-img-top" alt="..." />
-                            <div class="card-body">
-                                <h5 class="card-title">Lumi Vainio</h5>
-                                <p class="card-textMem">
-                                    Sihteeri<br />
-                                    Email: xxxx@tuni.fi
-                                </p>
-                            </div>
-                        </div>
-                        <div class="cardMem">
-                            <img src="..." class="card-img-top" alt="..." />
-                            <div class="card-body">
-                                <h5 class="card-title">Juuso Torikka</h5>
-                                <p class="card-textMem">
-                                    Koulutuspoliittinen Vastaava<br />
-                                    Email: xxxx@tuni.fi
-                                </p>
-                            </div>
-                        </div>
-                        <div class="cardMem">
-                            <img src="..." class="card-img-top" alt="..." />
-                            <div class="card-body">
-                                <h5 class="card-title">Mika Haapanen</h5>
-                                <p class="card-textMem">
-                                    Tapahtumavastaava<br />
-                                    Email: xxxx@tuni.fi
-                                </p>
+                            <div className='face face2'>
+                                <div className="memberContent">
+                                    <p>Rahastonhoitaja<br />
+                                        Email: xxxx@tuni.fi</p>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div className="card-group" style={{ marginTop: '50px' }}>
-                        <div class="cardMem">
-                            <img src="..." class="card-img-top" alt="..." />
-                            <div class="card-body">
-                                <h5 class="card-title">Enni Heinonen </h5>
-                                <p class="card-textMem">
-                                    Yhteistyövastaava<br />
-                                    Email: xxxx@tuni.fi
-                                </p>
+                    <div className="card-group-members">
+                        <div className="cardMem">
+                            <div className='face face1'>
+                                <div className="memberContent">
+                                    <img src={Image} className="img-fluid rounded-start" alt="..." />
+                                    <h3>Lumi Vainio</h3>
+                                </div>
+                            </div>
+                            <div className='face face2'>
+                                <div className="memberContent">
+                                    <p>Sihteeri<br />
+                                        Email: xxxx@tuni.fi</p>
+                                </div>
                             </div>
                         </div>
-                        <div class="cardMem">
-                            <img src="..." class="card-img-top" alt="..." />
-                            <div class="card-body">
-                                <h5 class="card-title">Markus Korpela</h5>
-                                <p class="card-textMem">
-                                    Tila- ja Jäsenvastaava<br />
-                                    Email: xxxx@tuni.fi
-                                </p>
+                        <div className="cardMem">
+                            <div className='face face1'>
+                                <div className="memberContent">
+                                    <img src={Image} className="img-fluid rounded-start" alt="..." />
+                                    <h3>Juuso Torikka</h3>
+                                </div>
+                            </div>
+                            <div className='face face2'>
+                                <div className="memberContent">
+                                    <p>Koulutuspoliittinen Vastaava<br />
+                                        Email: xxxx@tuni.fi</p>
+                                </div>
                             </div>
                         </div>
-                        <div class="cardMem">
-                            <img src="..." class="card-img-top" alt="..." />
-                            <div class="card-body">
-                                <h5 class="card-title">Sampo Mykkänen</h5>
-                                <p class="card-textMem">
-                                    Somevastaava<br />
-                                    Email: xxxx@tuni.fi
-                                </p>
+                        <div className="cardMem">
+                            <div className='face face1'>
+                                <div className="memberContent">
+                                    <img src={Image} className="img-fluid rounded-start" alt="..." />
+                                    <h3>Mika Haapanen</h3>
+                                </div>
+                            </div>
+                            <div className='face face2'>
+                                <div className="memberContent">
+                                    <p>Tapahtumavastaava<br />
+                                        Email: xxxx@tuni.fi</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card-group-members">
+                        <div className="cardMem">
+                            <div className='face face1'>
+                                <div className="memberContent">
+                                    <img src={Image} className="img-fluid rounded-start" alt="..." />
+                                    <h3>Enni Heinonen</h3>
+                                </div>
+                            </div>
+                            <div className='face face2'>
+                                <div className="memberContent">
+                                    <p>Yhteistyövastaava<br />
+                                        Email: xxxx@tuni.fi</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="cardMem">
+                            <div className='face face1'>
+                                <div className="memberContent">
+                                    <img src={Image} className="img-fluid rounded-start" alt="..." />
+                                    <h3>Markus Korpela</h3>
+                                </div>
+                            </div>
+                            <div className='face face2'>
+                                <div className="memberContent">
+                                    <p>Tila-ja Jäsenvastaava<br />
+                                        Email: xxxx@tuni.fi</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="cardMem">
+                            <div className='face face1'>
+                                <div className="memberContent">
+                                    <img src={Image} className="img-fluid rounded-start" alt="..." />
+                                    <h3>Sampo Mykkänen</h3>
+                                </div>
+                            </div>
+                            <div className='face face2'>
+                                <div className="memberContent">
+                                    <p>Somevastaava<br />
+                                        Email: xxxx@tuni.fi</p>
+                                </div>
                             </div>
                         </div>
                     </div>
