7aa0d8a053d391fd108bbac2a8b83b3b9d9836b3
HNDuy Nguyen <duynguyen11062001@gmail.com>
2022-05-18 14:55:49 +0300

    Edit about page, add json data

--- taso/src/components/About/AboutE.js
+++ taso/src/components/About/AboutE.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './About.css'
 import Image from '../../images/card-image-top.png'
+import members from '../members.json'
 
 const AboutE = () => {
     return (
@@ -24,139 +25,27 @@ const AboutE = () => {
                         <h1>Members</h1>
                     </div>
                     <div className="card-group-members">
-                        <div className="cardMem">
-                            <div className='face face1'>
-                                <div className="memberContent">
-                                    <img src={Image} className="img-fluid rounded-start" alt="..." />
-                                    <h3>Matti Hämelahti</h3>
+                        {members.map(post => {
+                            return (
+                                <div className="cardMem" >
+                                    <div className='face face1'>
+                                        <div className="memberContent">
+                                            <img src={Image} className="img-fluid rounded-start" alt="..." />
+                                            <h3>{post.name}</h3>
+                                        </div>
+                                        <div className='face face2'>
+                                            <div className="memberContent">
+                                                <p>{post.title}<br />
+                                                    {post.email}</p>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className='face face2'>
-                                <div className="memberContent">
-                                    <p>Chairman<br />
-                                        Email: xxxx@tuni.fi</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="cardMem">
-                            <div className='face face1'>
-                                <div className="memberContent">
-                                    <img src={Image} className="img-fluid rounded-start" alt="..." />
-                                    <h3>Juuse Hannula</h3>
-                                </div>
-                            </div>
-                            <div className='face face2'>
-                                <div className="memberContent">
-                                    <p>Vice Chairman<br />
-                                        Email: xxxx@tuni.fi</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="cardMem">
-                            <div className='face face1'>
-                                <div className="memberContent">
-                                    <img src={Image} className="img-fluid rounded-start" alt="..." />
-                                    <h3>Samuli Marttila</h3>
-                                </div>
-                            </div>
-                            <div className='face face2'>
-                                <div className="memberContent">
-                                    <p>Treasurer<br />
-                                        Email: xxxx@tuni.fi</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="card-group-members">
-                        <div className="cardMem">
-                            <div className='face face1'>
-                                <div className="memberContent">
-                                    <img src={Image} className="img-fluid rounded-start" alt="..." />
-                                    <h3>Lumi Vainio</h3>
-                                </div>
-                            </div>
-                            <div className='face face2'>
-                                <div className="memberContent">
-                                    <p>Secretary<br />
-                                        Email: xxxx@tuni.fi</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="cardMem">
-                            <div className='face face1'>
-                                <div className="memberContent">
-                                    <img src={Image} className="img-fluid rounded-start" alt="..." />
-                                    <h3>Juuso Torikka</h3>
-                                </div>
-                            </div>
-                            <div className='face face2'>
-                                <div className="memberContent">
-                                    <p>Educational Affairs<br />
-                                        Email: xxxx@tuni.fi</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="cardMem">
-                            <div className='face face1'>
-                                <div className="memberContent">
-                                    <img src={Image} className="img-fluid rounded-start" alt="..." />
-                                    <h3>Mika Haapanen</h3>
-                                </div>
-                            </div>
-                            <div className='face face2'>
-                                <div className="memberContent">
-                                    <p>Events Coordinator<br />
-                                        Email: xxxx@tuni.fi</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="card-group-members">
-                        <div className="cardMem">
-                            <div className='face face1'>
-                                <div className="memberContent">
-                                    <img src={Image} className="img-fluid rounded-start" alt="..." />
-                                    <h3>Enni Heinonen</h3>
-                                </div>
-                            </div>
-                            <div className='face face2'>
-                                <div className="memberContent">
-                                    <p>Cooperations Coordinator<br />
-                                        Email: xxxx@tuni.fi</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="cardMem">
-                            <div className='face face1'>
-                                <div className="memberContent">
-                                    <img src={Image} className="img-fluid rounded-start" alt="..." />
-                                    <h3>Markus Korpela</h3>
-                                </div>
-                            </div>
-                            <div className='face face2'>
-                                <div className="memberContent">
-                                    <p>Facilities and Members Coordinator<br />
-                                        Email: xxxx@tuni.fi</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="cardMem">
-                            <div className='face face1'>
-                                <div className="memberContent">
-                                    <img src={Image} className="img-fluid rounded-start" alt="..." />
-                                    <h3>Sampo Mykkänen</h3>
-                                </div>
-                            </div>
-                            <div className='face face2'>
-                                <div className="memberContent">
-                                    <p>Social Media Coordinator<br />
-                                        Email: xxxx@tuni.fi</p>
-                                </div>
-                            </div>
-                        </div>
+                            )
+                        })}
                     </div>
                 </div>
-                <div className="col-md-12 membersView2">
+                {/* <div className="col-md-12 membersView2">
                     <div id="carouselMembers" class="carousel carousel-dark slide" data-bs-ride="carousel">
                         <div class="carousel-inner">
                             <div class="carousel-item active">
@@ -306,7 +195,7 @@ const AboutE = () => {
                         </div>
                         
                     </div>
-                </div>
+                </div> */}
             </div>
         </div>
     )
